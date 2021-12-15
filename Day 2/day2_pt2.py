969d8ca82c50fd88662c5be3d57409725a0f3730
bellcha <bellcha@gmail.com>
2021-12-14 19:34:29 -0600

    init

---
+++ Day 2/day2_pt2.py
@@ -0,0 +1,30 @@
+def main(commands):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for i in commands:
+        if i[0] == 'forward':
+            horizontal += int(i[1])
+            depth += (aim * int(i[1]))
+            print(i)
+            print (f'horizontal:{horizontal}\ndepth: {depth} \naim:{aim}')
+            print('-'*10)
+        elif i[0] == 'down':
+            aim += int(i[1])
+            print(i)
+            print (f'horizontal:{horizontal}\ndepth: {depth}\naim:{aim}')
+            print('-'*10)
+        elif i[0] == 'up':
+            aim -= int(i[1])
+            print(i)
+            print (f'horizontal:{horizontal}\ndepth: {depth}\naim:{aim}')
+            print('-'*10)
+    print(f'Final Value: {(horizontal * depth)}')
+
+
+if __name__ == "__main__":
+    with open ('input.txt', 'r') as f:
+        lines = f.read().splitlines()
+        commands = list(i.split(' ') for i in lines)
+
+    main(commands)
