dcde65ed410aa7525ede7499d16723a064d4583c
bellcha <bellcha@gmail.com>
2021-12-14 19:47:00 -0600

    printing final answer in main func

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -8,7 +8,7 @@ def main(commands):
             depth += int(i[1])
         elif i[0] == 'up':
             depth -= int(i[1])
-    return (horizontal * depth)
+    print(f'Final Value: {(horizontal * depth)}')
 
 
 if __name__ == "__main__":
@@ -16,4 +16,4 @@ if __name__ == "__main__":
         lines = f.read().splitlines()
         commands = list(i.split(' ') for i in lines)
 
-    print(main(commands))
+    main(commands)
