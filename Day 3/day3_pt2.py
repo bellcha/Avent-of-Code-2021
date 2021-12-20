ba853eeae098c04c0adc9aeedc02283bdda6895e
bellcha <bellcha@gmail.com>
2021-12-19 20:52:25 -0600

    ugly but works

--- Day 3/day3_pt2.py
+++ Day 3/day3_pt2.py
@@ -1,10 +1,41 @@
-#test input works but not problem input
+#This is ugly but works.
 
 def most_freq(List):
-    return max(set(List), key=List.count)
+
+    ones = 0
+    zeros = 0
+    print(List)
+
+    for i in List:
+        if i == '1':
+            ones +=1
+        else:
+            zeros += 1
+        
+    if ones == zeros:
+        return '1'
+    elif ones > zeros:
+        return '1'
+    else:
+        return '0'
 
 def least_freq(List):
-    return min(set(List), key=List.count)
+    ones = 0
+    zeros = 0
+    print(List)
+
+    for i in List:
+        if i == '1':
+            ones +=1
+        else:
+            zeros += 1
+        
+    if zeros == ones:
+        return '0'
+    elif zeros < ones:
+        return '0'
+    else:
+        return '1'
 
 def oxygen_rating(input):
 
@@ -22,7 +53,8 @@ def oxygen_rating(input):
             if line[x] != top:
                 lines.remove(line)
 
-    return (int(''.join(lines), 2))
+
+    return int(''.join(lines), 2), lines
 
 def co_rating(input):
 
@@ -31,16 +63,19 @@ def co_rating(input):
 
     for x in range(len(lines[0])):
 
-        test_line = [line[x] for line in lines]
+        if len(lines) > 1:
 
-        top = least_freq(test_line)
+            test_line = [line[x] for line in lines]
 
-        for line in lines[:]:
-            if line[x] != top:
-                lines.remove(line)
+            top = least_freq(test_line)
+
+            for line in lines[:]:
+                if line[x] != top:
+                    lines.remove(line)
+        else:
+            break
     
-    return int(''.join(lines), 2)
-
+    return int(''.join(lines), 2), lines
 
 
 def life_support(input):
@@ -50,12 +85,8 @@ def life_support(input):
     y = co_rating(input)
     
     print(x, y)
+    print (x[0] * y[0])
 
-            
-
-def main():
-
-    life_support('input.txt')
 
 if __name__ == '__main__':
-    main()
+    life_support('input.txt')
